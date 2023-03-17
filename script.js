0bd3290d5ac168f0544fc9ecc5876fe371e2d9f3
Kena Njonge <kenakiruri@gmail.com>
2023-03-17 04:45:28 +0100

    JavaScript is working

--- script.js
+++ script.js
@@ -1,20 +1,20 @@
 var ratingButtons = document.getElementsByClassName("rating");
-var ratingObject = {button1: 0, button2: 0, button3: 0, button4: 0, button5: 0};
-var ratingObjectarray = Object.entries(ratingObject);
 
 function selRating(val){
-    for(let i=0; i<5; i++){
-        if(ratingButtons[i][1]==val){
-            ratingButtons[i][1].style.opacity= 0.5;
-            ratingButtons[i][1].style.borderWidth="2px";
-            ratingButtons[i][1].style.borderStyle="solid"
-            ratingButtons[i][1].style.borderColor="var(--Lgrey)"
-            for(let j=0;j<5; j++){
-                if(ratingButtons[i][1]!=ratingButtons[j][1]){
-                    ratingButtons[j][1]= 0.5;
-                    ratingObjectarray[j][1].style.opacity= 1;  
-                }
-            }
+    for(let i=0;i<5;i++){
+        if(val==ratingButtons[i]){
+            console.log(i);
+        }
+
+    }
+    val.style.opacity= 0.5;
+    val.style.borderWidth="2px";
+    val.style.borderColor="var(--Dblue)";
+    val.style.borderStyle="solid";
+    for(let j=0;j<5; j++){
+        if(ratingButtons[j]!=val){
+            ratingButtons[j].style.opacity= 1;
+            ratingButtons[j].style.borderWidth="0px";
         }
     }
 }
@@ -22,10 +22,10 @@ function selRating(val){
 
 function cont(){
     for(let i=0; i<5;i++){
-        if(ratingButtons[i][1]==1){
-            let page1 = document.getElementsByClassName("request");
+        if(ratingButtons[i].style.borderWidth=="2px"){
+            let page1 = document.getElementById("page1");
             page1.style.display = "none";
-            let page2 = document.getElementsByClassName("submited");
+            let page2 = document.getElementById("page2");
             page2.style.display = "flex";
             let response = document.getElementById("thank1");
             let value = i+ 1;
